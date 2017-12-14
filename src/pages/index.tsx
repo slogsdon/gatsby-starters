90fcbd8850bfbb9ca6167f797db4c45f6e2fd85c
Shane Logsdon <shane@shanelogsdon.com>
2017-12-14 15:31:18 -0500

    add graphql example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,34 @@
 import * as React from "react";
 
-export default () => <div>Hello world!</div>;
+// Please note that you can use https://github.com/dotansimha/graphql-code-generator
+// to generate all types from graphQL schema
+interface IndexPageProps {
+  data: {
+    site: {
+      siteMetadata: {
+        siteName: string;
+      };
+    };
+  };
+}
+
+export default class extends React.Component<IndexPageProps> {
+  public render() {
+    return (
+      <div>
+        <h1>{this.props.data.site.siteMetadata.siteName}</h1>
+        <p>Hello world!</p>
+      </div>
+    );
+  }
+}
+
+export const pageQuery = graphql`
+query IndexQuery {
+  site {
+    siteMetadata {
+      siteName
+    }
+  }
+}
+`;
